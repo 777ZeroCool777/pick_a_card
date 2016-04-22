01594495e2f64d8a5fc30016ca85b378d1c86907
ZeroCool <exwarvlad@gmail.com>
2016-04-22 14:23:51 +0300

    version 2

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -3,4 +3,4 @@
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'T']
 suits = ['♦', '♥', '♣', '♠'] # Буби, Черви, Крести, Пики
 
-puts values.sample  suits.sample
+puts values.sample + suits.sample
